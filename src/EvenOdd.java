509403663384486617b2135381f379405f55a8d8
walsh2am <walsh2am@mail.uc.edu>
2026-02-07 11:52:20 -0500

    Completed Code

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,6 +2,17 @@ public class EvenOdd
 {
     public static void main(String[] args)
     {
-        
+        int numToExamine = 210;
+        int examine;
+        examine = numToExamine % 2;
+
+        if (examine == 0)
+        {
+            IO.println("The number is even.");
+        }
+        else
+        {
+            IO.println("The number is odd.");
+        }
     }
 }
